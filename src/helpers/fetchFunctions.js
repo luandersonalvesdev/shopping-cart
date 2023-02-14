fd95310619adfb8ea4aad6bbe4ad9b0437ccd280
Luanderson Alves <luaoderson@gmail.com>
2023-02-14 17:50:01 -0300

    requirement 02

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,11 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = (query) => {
+  if (!query) {
+    throw new Error('Termo de busca não informado');
+  }
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((response) => response.json())
+    .then((data) => data.results);
 };
