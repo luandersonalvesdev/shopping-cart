dfafb7f06c955f5c186776d9fc21c0254499d222
Luanderson Alves <luaoderson@gmail.com>
2023-02-14 18:04:15 -0300

    requirement 03

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
+const fShowProductsOnScreen = async () => {
+  const sectionProductsEl = document.querySelector('section .products');
+  const arrProducts = await fetchProductsList('computador');
+  arrProducts.forEach((product) => {
+    sectionProductsEl.appendChild(createProductElement(product));
+  });
+};
+
+fShowProductsOnScreen();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
