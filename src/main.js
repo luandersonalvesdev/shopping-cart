4058bac2c9aac868e49c13a778290915aac95936
Luanderson Alves <luaoderson@gmail.com>
2023-02-15 11:41:55 -0300

    requirement 05

--- src/main.js
+++ src/main.js
@@ -3,21 +3,41 @@ import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
+const gSectionProductsEl = document.querySelector('section .products');
+
+const fVerifyResponseApi = async () => {
+  const response = await fetchProductsList('computador');
+  if (Array.isArray(response)) {
+    return response;
+  }
+  return false;
+};
+
+const fLoadingError = () => {
+  const gSpanEl = document.querySelector('.loading');
+  gSpanEl.className = 'error';
+  gSpanEl.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+}
+
 const fLoading = () => {
-  const cDivEl = document.createElement('span');
-  cDivEl.innerHTML = 'carregando...';
-  cDivEl.className = 'loading';
-  return cDivEl;
+  gSectionProductsEl.innerHTML = '';
+  const cSpanEl = document.createElement('span');
+  cSpanEl.className = 'loading';
+  cSpanEl.innerHTML = 'carregando...';
+  gSectionProductsEl.appendChild(cSpanEl);
 };
 
 const fShowProductsOnScreen = async () => {
-  const gSectionProductsEl = document.querySelector('section .products');
-  gSectionProductsEl.appendChild(fLoading());
-  const arrProducts = await fetchProductsList('computador');
-  gSectionProductsEl.innerHTML = '';
-  arrProducts.forEach((product) => {
-    gSectionProductsEl.appendChild(createProductElement(product));
-  });
+  try {
+    fLoading();
+    const responseApi = await fetchProductsList('computador');
+    gSectionProductsEl.innerHTML = '';
+    responseApi.forEach((product) => {
+      gSectionProductsEl.appendChild(createProductElement(product));
+    });
+  } catch (e) {
+    fLoadingError();
+  }
 };
 
 fShowProductsOnScreen();
